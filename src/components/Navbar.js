1c52504abc881c805f6cae7936e6fd3341e9d147
Shakeel26 <xhakil26@gmail.com>
2021-09-08 02:19:47 +0200

    designing of some components

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Link from "@material-ui/core/Link";
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyles = makeStyles(theme => ({
+  mainContainer: {
+    backgroundColor: 'yellow',
+  },
+  menuItems: {
+    alignItems: 'center'
+  }
+}))
+
+
+function Navbar() {
+  const classes = useStyles();
+
+  return (
+
+    <AppBar className={classes.mainContainer}>
+      <Toolbar className={classes.menuItems}>
+        <MenuItem>
+          <Link href="#about" >About</Link>
+        </MenuItem>
+        <MenuItem>
+          <Link href="#skills" >Skills</Link>
+        </MenuItem>
+        <MenuItem>
+          <Link href="#projects" >Projects</Link>
+        </MenuItem>
+        <MenuItem>
+          <Link href="#testimonials" >Testimonials</Link>
+        </MenuItem>
+        <MenuItem>
+          <Link href="#Contact" >Contact</Link>
+        </MenuItem>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar;
